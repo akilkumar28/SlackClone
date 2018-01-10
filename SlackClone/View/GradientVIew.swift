ce1cfc6dac659effdde5a5407727533c59766b1b
akilkumar28 <akil.psn@gmail.com>
2018-01-10 17:15:06 +0530

    created a custom gradient view

---
+++ SlackClone/View/GradientVIew.swift
@@ -0,0 +1,36 @@
+//
+//  GradientVIew.swift
+//  SlackClone
+//
+//  Created by AKIL KUMAR THOTA on 1/10/18.
+//  Copyright © 2018 AKIL KUMAR THOTA. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientVIew: UIView {
+
+    @IBInspectable var topColor:UIColor = UIColor.red {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor:UIColor = UIColor.black {
+        didSet{
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.colors = [topColor.cgColor,bottomColor.cgColor]
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
